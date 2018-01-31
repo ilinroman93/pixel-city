2a3445e0f987c6e0eae454d2d06bb7236111a120
Roman Ilin <ilrose887@gmail.com>
2018-01-31 16:14:24 +0800

    collection view added

---
+++ pixel-city/pixel-city/View/PhotoCell.swift
@@ -0,0 +1,21 @@
+//
+//  PhotoCell.swift
+//  pixel-city
+//
+//  Created by Ильин Роман on 31.01.2018.
+//  Copyright © 2018 Romax. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
